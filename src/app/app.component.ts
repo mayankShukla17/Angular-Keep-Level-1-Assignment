74513bf79d69f860f5b56157a040a4325492ef1b
Ubuntu <ubuntu@ip-172-31-43-60.ap-south-1.compute.internal>
2020-10-07 20:33:44 +0000

    Angular Keep Level 1 Assignment Completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
+import { Note } from './note';
+import { NotesService } from './notes.service';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +9,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
   errMessage: string;
+  note: Note = new Note();
+  notes: Array<Note> = [];
+
+  constructor(private noteService: NotesService) { }
+
+  ngOnInit() {
+    this.noteService.getNotes().subscribe(
+      notes => this.notes = notes,
+      err => this.errMessage = err.message
+    );
+  }
+
+  takeNotes() {
+    if (this.note.title && this.note.text) {
+      this.notes.push(this.note);
+      this.noteService.addNote(this.note).subscribe(
+        data => { },
+        err => {
+          const index: number =
+            this.notes.findIndex(note => note.title === this.note.title);
+          this.notes.splice(index, 1);
+          this.errMessage = err.message;
+        }
+      );
+      this.note = new Note();
+    } else {
+      this.errMessage = 'Title and Text both are required fields';
+    }
+  }
 
 }
